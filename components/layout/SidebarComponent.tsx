2e092560b67b6ec1cd247fec7c149b2c5e2eeadb
techiemokhele <neomokhele23@gmail.com>
2024-05-10 21:10:09 +0200

    implement sidebar content

--- components/layout/SidebarComponent.tsx
+++ components/layout/SidebarComponent.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+//custom
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
 
 const SidebarComponent = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const pathname = usePathname();
 
-export default SidebarComponent
+  return (
+    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+      <div className="flex flex-col flex-1 gap-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            pathname === link.route || pathname.startsWith(link.route);
+
+          return (
+            <Link
+              key={link.label}
+              href={link.route}
+              className={cn(
+                "flex gap-4 items-center p-4 rounded-lg justify-start",
+                {
+                  "bg-blue-1": isActive,
+                }
+              )}
+            >
+              <Image
+                src={link.imgUrl}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+
+              <p className="tex-lg font-semibold max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default SidebarComponent;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,9 @@ const config = {
         dark: {
           1: "#1C1F2E",
           2: "#161925",
+        },  
+        blue: {
+          1: "#0E78F9",
         },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
